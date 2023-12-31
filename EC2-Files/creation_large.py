c5452722502959310b51c09aa30ae8fda282e795
BFaras <mohamed.ben-faras@polymtl.ca>
2023-12-31 05:32:48 -0500

    proxy pattern start implementation

---
+++ EC2-Files/creation_large.py
@@ -0,0 +1,28 @@
+# Imports
+import boto3
+from credentials import aws_access_key_id, aws_secret_access_key, aws_session_token, aws_region
+from security_information import security_group_name , security_group_description
+
+# AWS CLI configuration values
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    aws_session_token=aws_session_token,
+    region_name=aws_region
+)
+
+# Initialize the EC2 client
+ec2_client = session.client('ec2')
+
+# Launch t2.large EC2 instances for stand-alone
+t2_instance_response_alone = ec2_client.run_instances(
+    ImageId='ami-053b0d53c279acc90',
+    UserData=open('proxy_config.sh').read(),
+    PrivateIpAddress = '172.31.30.30',
+    InstanceType='t2.large',
+    MinCount=1,
+    MaxCount=1,
+    Placement={'AvailabilityZone': 'us-east-1a'},
+    KeyName='finalProject',
+    SecurityGroups=[security_group_name]
+)
---
+++ EC2-Files/proxy_config.sh
@@ -0,0 +1,7 @@
+
+
+sudo apt update
+sudo apt install python3-pip -y
+sudo pip install sshtunnel 
+sudo pip install pythonping
+sudo pip install pymysql 
