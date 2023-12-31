306711eb82e9c852c3f2a71f0328efb85b487dbb
BFaras <mohamed.ben-faras@polymtl.ca>
2023-12-31 15:14:06 -0500

    add a proxy test that connects to master cluster

---
+++ EC2-Files/proxy-test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import paramiko
+
+jumpbox_public_addr = '18.215.158.107'
+jumpbox_private_addr = '172.31.30.30'
+target_addr = '172.31.30.10'
+
+private_key_path = paramiko.RSAKey.from_private_key_file("finalProject.pem")
+
+jumpbox=paramiko.SSHClient()
+jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+jumpbox.connect(jumpbox_public_addr, username='ubuntu', pkey=private_key_path)
+
+jumpbox_transport = jumpbox.get_transport()
+src_addr = (jumpbox_private_addr, 22)
+dest_addr = (target_addr, 22)
+jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip", dest_addr, src_addr)
+
+target=paramiko.SSHClient()
+target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+target.connect(target_addr, username='ubuntu', pkey= private_key_path, sock=jumpbox_channel)
+
+stdin, stdout, stderr = target.exec_command("cat clusterBenchmark.txt")
+for line in stdout.read().split(b'\n'):
+  print(str(line))
+
+target.close()
+jumpbox.close()
--- EC2-Files/proxy_hits.py
+++ EC2-Files/proxy_hits.py
@@ -1,11 +1,22 @@
 import sys
 import time
 import mysql.connector
+import paramiko
 import random
 
+def create_ssh_tunnel(ssh_host, ssh_username, ssh_key_path, target_host, target_port):
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh.connect(ssh_host, username=ssh_username, pkey=ssh_key_path)
+
+    tunnel = ssh.get_transport().open_channel(
+        'direct-tcpip', (target_host, target_port), ('127.0.0.1', 0)
+    )
+
+    return tunnel
 def execute_queries(ip, name, query,target):
-
-
 
     with mysql.connector.connect(
         host=target,
@@ -26,8 +37,20 @@ def execute_queries(ip, name, query,target):
 
 
 def direct_hit(query):
+     # SSH tunnel configuration
+    ip_host = '18.212.249.223'
+    ssh_username = 'ubuntu'
+    ssh_key_path = 'finalProject.pem'
+
+    # Target MySQL server configuration
+    target_host = 'ip-172-31-30-10.ec2.internal'
+    target_port = 3306
+
+    # Create an SSH tunnel
+    ssh_tunnel = create_ssh_tunnel(ip_host, ssh_username, ssh_key_path, target_host, target_port)
+
     target = "ip-172-31-30-10.ec2.internal"
-    execute_queries('127.0.0.1', 'master', query, target)
+    execute_queries('127.0.0.1', 'master', query, target_host)
 
 def random_hit(query):
     targets = ["ip-172-31-30-11.ec2.internal", "ip-172-31-30-12.ec2.internal","ip-172-31-30-13.ec2.internal"]
@@ -37,6 +60,6 @@ def random_hit(query):
 
 if __name__ == "__main__":
     SQL_query = sys.argv[1]
+    
     direct_hit(SQL_query)
-    time.sleep(45)
-    random_hit(SQL_query)
+
